703800333f6b27ee37304a4b491e35df03be76c4
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2019-12-17 23:27:36 +0300

    Add header and footer components

--- src/view-module/root.component.ts
+++ src/view-module/root.component.ts
@@ -1,52 +1,8 @@
 import { Component } from '@angular/core';
-import { RootStateModel, articlesActions, sourcesActions } from '@state';
-import { Store } from '@ngrx/store';
-import { LanguageEnum, ArticleModel, SourceDetailsModel, CountryEnum } from '@domain';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'news-root',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.scss']
 })
-export class AppComponent {
-  public constructor(private readonly store: Store<RootStateModel>) {
-    this.articles$ = this.store.select(state => state.articles);
-    this.sources$ = this.store.select(state => state.sources);
-  }
-
-  public readonly articles$: Observable<ArticleModel[]>;
-  public readonly sources$: Observable<SourceDetailsModel[]>;
-
-  public fetchTop(): void {
-    this.clear();
-    this.store.dispatch(articlesActions.searchTopArticles({
-      request: { language: LanguageEnum.english }
-    }));
-  }
-
-  public fetchEverything(): void {
-    this.clear();
-    this.store.dispatch(articlesActions.searchArticles({
-      request: {
-        language: LanguageEnum.english,
-        searchString: 'Microsoft'
-      }
-    }));
-  }
-
-  public fetchSources(): void {
-    this.clear();
-    this.store.dispatch(sourcesActions.searchSources({
-      request: {
-        language: LanguageEnum.english,
-        country: CountryEnum.unitesStatesOfAmerica
-      }
-    }));
-  }
-
-  private clear(): void {
-    this.store.dispatch(articlesActions.storeArticles({ articles: [] }));
-    this.store.dispatch(sourcesActions.storeSources({ sources: [] }));
-  }
-}
+export class AppComponent {}
--- src/view-module/view.module.ts
+++ src/view-module/view.module.ts
@@ -3,17 +3,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 import { StateModule } from '@state';
 import { AppComponent } from './root.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [];
 const materialModules = [
-  MatButtonModule
+  MatButtonModule,
+  MatSelectModule
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
