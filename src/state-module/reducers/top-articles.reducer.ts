2f0cbc54f19e0721c15f20b2193ef128f4877d78
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2020-01-01 12:51:57 +0300

    Remove duplicate articles

--- src/state-module/reducers/top-articles.reducer.ts
+++ src/state-module/reducers/top-articles.reducer.ts
@@ -1,6 +1,7 @@
 import { createReducer, Action, on } from '@ngrx/store';
 import { topArticlesActions } from '../actions/top-articles.actions';
 import { TopArticlesStateModel } from '../models/top-articles-state.model';
+import { ArticleModel } from '@domain';
 
 const reducer = createReducer<TopArticlesStateModel, Action>(
   {
@@ -8,7 +9,7 @@ const reducer = createReducer<TopArticlesStateModel, Action>(
     articles: null
   },
   on(topArticlesActions.storeArticles, (state, action) => {
-    return { ...state, articles: action.articles };
+    return { ...state, articles: distinctByUrl(action.articles) };
   }),
   on(topArticlesActions.storeFilter, (state, action) => {
     return { ...state, filter: action.filterState };
@@ -20,4 +21,10 @@ export function topArticlesReducer(
   action: Action
 ): TopArticlesStateModel {
   return reducer(state, action);
+}
+
+function distinctByUrl(articles: ArticleModel[]): ArticleModel[] {
+  const dictionary = {};
+  for (const article of articles) dictionary[article.url] = article;
+  return Object.keys(dictionary).map(key => dictionary[key]);
 }
