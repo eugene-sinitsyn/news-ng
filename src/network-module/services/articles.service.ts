3481cdb32da1012625a87d8f25c9a4235029cf25
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2020-01-01 19:30:26 +0300

    Refactoring: cleanup articles service, reduce tooltip delay

--- src/network-module/services/articles.service.ts
+++ src/network-module/services/articles.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpService } from '../http/http.service';
-import { TopArticlesRequestModel, ArticleModel, SearchArticlesRequestModel } from '@domain';
+import { TopArticlesRequestModel, ArticleModel, SearchArticlesRequestModel, LanguageEnum } from '@domain';
 import { environment } from '@environment';
 import { Injectable } from '@angular/core';
 import { ArticlesResponseModel } from '../models/articles-response.model';
@@ -13,10 +13,31 @@ export class ArticlesService extends HttpService {
   }
 
   public async fetchTop(request: TopArticlesRequestModel): Promise<ArticleModel[]> {
+    let params = this.createBaseParams(request.language);
+    params = this.setTopArticlesParams(params, request);
+    const response: ArticlesResponseModel = await this.httpGet('top-headlines', params);
+
+    if (response.status === ResponseStatus.error) throw new Error(response.message);
+    else return response.articles || [];
+  }
+
+  public async search(request: SearchArticlesRequestModel): Promise<ArticleModel[]> {
+    let params = this.createBaseParams(request.language);
+    params = this.setSearchArticlesParams(params, request);
+    const response: ArticlesResponseModel = await this.httpGet('everything', params);
+
+    if (response.status === ResponseStatus.error) throw new Error(response.message);
+    else return response.articles || [];
+  }
+
+  private createBaseParams(language: LanguageEnum): HttpParams {
     let params: HttpParams = new HttpParams()
       .set('apiKey', environment.apiKey)
-      .set('language', request.language);
+      .set('language', language);
+    return params;
+  }
 
+  private setTopArticlesParams(params: HttpParams, request: TopArticlesRequestModel): HttpParams {
     if (request.country) params = params.set('country', request.country);
     if (request.category) params = params.set('category', request.category);
     if (request.sources && request.sources.length)
@@ -26,17 +47,10 @@ export class ArticlesService extends HttpService {
     if (request.pageSize) params = params.set('pageSize', request.pageSize.toString());
     if (request.page) params = params.set('page', request.page.toString());
 
-    const response: ArticlesResponseModel = await this.httpGet('top-headlines', params);
-
-    if (response.status === ResponseStatus.error) throw new Error(response.message);
-    else return response.articles || [];
+    return params;
   }
 
-  public async search(request: SearchArticlesRequestModel): Promise<ArticleModel[]> {
-    let params: HttpParams = new HttpParams()
-      .set('apiKey', environment.apiKey)
-      .set('language', request.language);
-
+  private setSearchArticlesParams(params: HttpParams, request: SearchArticlesRequestModel): HttpParams {
     if (request.sources) params = params.set('sources', request.sources.join(','));
     if (request.searchString) params = params.set(
       request.searchInTitleOnly ? 'qInTitle' : 'q',
@@ -48,9 +62,6 @@ export class ArticlesService extends HttpService {
     if (request.page) params = params.set('page', request.page.toString());
     if (request.sortBy) params = params.set('sortBy', request.sortBy);
 
-    const response: ArticlesResponseModel = await this.httpGet('everything', params);
-
-    if (response.status === ResponseStatus.error) throw new Error(response.message);
-    else return response.articles || [];
+    return params;
   }
 }
--- src/view-module/config/view-config.ts
+++ src/view-module/config/view-config.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ViewConfiguration {
-  public readonly tooltipDelay: number = 1000 // ms
+  public readonly tooltipDelay: number = 500 // ms
 }
