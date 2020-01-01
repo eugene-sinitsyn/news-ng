f0021d23baa7954948afc180f29b9b25536f73c8
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2020-01-01 14:22:28 +0300

    Fix: fetch top articles on language change

--- src/view-module/pages/top-articles/top-articles.component.ts
+++ src/view-module/pages/top-articles/top-articles.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
+import { skip } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { ArticleModel } from '@domain';
 import { RootStateModel, topArticlesActions } from '@state';
@@ -22,6 +23,9 @@ export class TopArticlesComponent implements OnInit, OnDestroy {
   public ngOnInit(): void {
     this.subscription = this.store.select(state => state.top.articles)
       .subscribe(articles => this.articles = articles);
+    this.subscription.add(this.store.select(state => state.preferences.language)
+      .pipe(skip(1))
+      .subscribe(() => this.store.dispatch(topArticlesActions.fetchArticles())));
 
     if (!this.articles) this.store.dispatch(topArticlesActions.fetchArticles());
   }
