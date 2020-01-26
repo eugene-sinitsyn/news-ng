efa6a89916e32ceb8263d8df1b7ba4cb1658ba8d
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2020-01-26 10:46:05 +0300

    Display saved articles on "read later" page

--- src/view-module/pages/read-later/read-later.component.ts
+++ src/view-module/pages/read-later/read-later.component.ts
@@ -1,7 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { ArticleModel } from '@domain';
+import { RootStateModel, readLaterActions } from '@state';
 
 @Component({
   selector: 'news-read-later',
   templateUrl: './read-later.component.html'
 })
-export class ReadLaterComponent {}
+export class ReadLaterComponent implements OnInit, OnDestroy {
+  public constructor(private readonly store: Store<RootStateModel>) {}
+
+  private readonly subscription: Subscription = new Subscription;
+  public articles: ArticleModel[];
+
+  public ngOnInit(): void {
+    if (!this.articles)
+      this.store.dispatch(readLaterActions.loadReadLaterArticles());
+
+    this.subscription.add(
+      this.store.select(state => state.readLater)
+        .subscribe(articles => this.articles = articles)
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
