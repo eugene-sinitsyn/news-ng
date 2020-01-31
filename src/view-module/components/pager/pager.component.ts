741c046fb7652730657f32cca7338feb2bd3cedd
Eugene Sinitsyn <e.sinitsyn.s@gmail.com>
2020-01-31 15:17:12 +0300

    Setup "pager" component API

--- src/view-module/components/pager/pager.component.ts
+++ src/view-module/components/pager/pager.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'news-pager',
   templateUrl: './pager.component.html',
   styleUrls: ['./pager.component.scss']
 })
-export class PagerComponent {}
+export class PagerComponent {
+  @Input() public loaded: number = 0;
+  @Input() public total: number = 0;
+  @Output() public readonly loadMore: EventEmitter<void> = new EventEmitter<void>();
+
+  public get status(): { loaded: number; total: number } {
+    return { loaded: this.loaded, total: this.total };
+  }
+
+  public get loadAvailable(): boolean {
+    return this.total && this.loaded < this.total;
+  }
+}
